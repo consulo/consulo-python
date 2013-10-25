6564ef32dc4ab35fe835b2c5345d539dd0e78063
VISTALL <vistall.valeriy@gmail.com>
2013-10-25 18:05:57 +0300

    licnce

--- python-psi-api/src/com/jetbrains/python/module/PyModuleService.java
+++ python-psi-api/src/com/jetbrains/python/module/PyModuleService.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.jetbrains.python.module;
 
 import com.intellij.ide.util.projectWizard.ModuleBuilder;
