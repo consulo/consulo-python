4622a9b07ecfd6e433421b941977d77adbb750da
VISTALL <vistall.valeriy@gmail.com>
2013-11-12 12:07:53 +0200

    hide run type if no extensions

--- python-impl/src/com/jetbrains/python/testing/PythonTestConfigurationType.java
+++ python-impl/src/com/jetbrains/python/testing/PythonTestConfigurationType.java
@@ -16,7 +16,15 @@
 
 package com.jetbrains.python.testing;
 
-import com.intellij.execution.configurations.*;
+import javax.swing.Icon;
+
+import org.consulo.python.module.extension.PyModuleExtension;
+import org.jetbrains.annotations.NotNull;
+import org.mustbe.consulo.module.extension.ModuleExtensionHelper;
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.configurations.ConfigurationType;
+import com.intellij.execution.configurations.ConfigurationTypeUtil;
+import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.python.PyBundle;
 import com.jetbrains.python.testing.attest.PythonAtTestRunConfiguration;
@@ -25,130 +33,189 @@ import com.jetbrains.python.testing.nosetest.PythonNoseTestRunConfiguration;
 import com.jetbrains.python.testing.pytest.PyTestRunConfiguration;
 import com.jetbrains.python.testing.unittest.PythonUnitTestRunConfiguration;
 import icons.PythonIcons;
-import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
 
 /**
  * User : catherine
  */
-public class PythonTestConfigurationType implements ConfigurationType {
-  public static final String ID = "tests";
+public class PythonTestConfigurationType implements ConfigurationType
+{
+	public static final String ID = "tests";
 
-  public final PythonDocTestConfigurationFactory PY_DOCTEST_FACTORY = new PythonDocTestConfigurationFactory(this);
-  public final PythonUnitTestConfigurationFactory PY_UNITTEST_FACTORY = new PythonUnitTestConfigurationFactory(this);
-  public final PythonNoseTestConfigurationFactory PY_NOSETEST_FACTORY = new PythonNoseTestConfigurationFactory(this);
-  public final PythonPyTestConfigurationFactory PY_PYTEST_FACTORY = new PythonPyTestConfigurationFactory(this);
-  public final PythonAtTestConfigurationFactory PY_ATTEST_FACTORY = new PythonAtTestConfigurationFactory(this);
+	public final PythonDocTestConfigurationFactory PY_DOCTEST_FACTORY = new PythonDocTestConfigurationFactory(this);
+	public final PythonUnitTestConfigurationFactory PY_UNITTEST_FACTORY = new PythonUnitTestConfigurationFactory(this);
+	public final PythonNoseTestConfigurationFactory PY_NOSETEST_FACTORY = new PythonNoseTestConfigurationFactory(this);
+	public final PythonPyTestConfigurationFactory PY_PYTEST_FACTORY = new PythonPyTestConfigurationFactory(this);
+	public final PythonAtTestConfigurationFactory PY_ATTEST_FACTORY = new PythonAtTestConfigurationFactory(this);
 
-  public static PythonTestConfigurationType getInstance() {
-    return ConfigurationTypeUtil.findConfigurationType(PythonTestConfigurationType.class);
-  }
+	public static PythonTestConfigurationType getInstance()
+	{
+		return ConfigurationTypeUtil.findConfigurationType(PythonTestConfigurationType.class);
+	}
 
-  private static class PythonUnitTestConfigurationFactory extends ConfigurationFactory {
-    protected PythonUnitTestConfigurationFactory(ConfigurationType configurationType) {
-      super(configurationType);
-    }
+	private static class PythonUnitTestConfigurationFactory extends ConfigurationFactory
+	{
+		protected PythonUnitTestConfigurationFactory(ConfigurationType configurationType)
+		{
+			super(configurationType);
+		}
 
-    @Override
-    public RunConfiguration createTemplateConfiguration(Project project) {
-      return new PythonUnitTestRunConfiguration(project, this);
-    }
+		@Override
+		public RunConfiguration createTemplateConfiguration(Project project)
+		{
+			return new PythonUnitTestRunConfiguration(project, this);
+		}
 
-    @Override
-    public String getName() {
-      return PyBundle.message("runcfg.unittest.display_name");
-    }
-  }
+		@Override
+		public boolean isApplicable(@NotNull Project project)
+		{
+			return ModuleExtensionHelper.getInstance(project).hasModuleExtension(PyModuleExtension.class);
+		}
 
-  private static class PythonDocTestConfigurationFactory extends ConfigurationFactory {
-    protected PythonDocTestConfigurationFactory(ConfigurationType configurationType) {
-      super(configurationType);
-    }
+		@Override
+		public String getName()
+		{
+			return PyBundle.message("runcfg.unittest.display_name");
+		}
+	}
 
-    @Override
-    public RunConfiguration createTemplateConfiguration(Project project) {
-      return new PythonDocTestRunConfiguration(project, this);
-    }
+	private static class PythonDocTestConfigurationFactory extends ConfigurationFactory
+	{
+		protected PythonDocTestConfigurationFactory(ConfigurationType configurationType)
+		{
+			super(configurationType);
+		}
 
-    @Override
-    public String getName() {
-      return PyBundle.message("runcfg.doctest.display_name");
-    }
-  }
+		@Override
+		public RunConfiguration createTemplateConfiguration(Project project)
+		{
+			return new PythonDocTestRunConfiguration(project, this);
+		}
 
-  private static class PythonPyTestConfigurationFactory extends ConfigurationFactory {
-    protected PythonPyTestConfigurationFactory(ConfigurationType configurationType) {
-      super(configurationType);
-    }
+		@Override
+		public boolean isApplicable(@NotNull Project project)
+		{
+			return ModuleExtensionHelper.getInstance(project).hasModuleExtension(PyModuleExtension.class);
+		}
 
-    @Override
-    public RunConfiguration createTemplateConfiguration(Project project) {
-      return new PyTestRunConfiguration(project, this);
-    }
+		@Override
+		public String getName()
+		{
+			return PyBundle.message("runcfg.doctest.display_name");
+		}
+	}
 
-    @Override
-    public String getName() {
-      return PyBundle.message("runcfg.pytest.display_name");
-    }
-  }
+	private static class PythonPyTestConfigurationFactory extends ConfigurationFactory
+	{
+		protected PythonPyTestConfigurationFactory(ConfigurationType configurationType)
+		{
+			super(configurationType);
+		}
 
-  private static class PythonNoseTestConfigurationFactory extends ConfigurationFactory {
-    protected PythonNoseTestConfigurationFactory(ConfigurationType configurationType) {
-      super(configurationType);
-    }
+		@Override
+		public RunConfiguration createTemplateConfiguration(Project project)
+		{
+			return new PyTestRunConfiguration(project, this);
+		}
 
-    @Override
-    public RunConfiguration createTemplateConfiguration(Project project) {
-      return new PythonNoseTestRunConfiguration(project, this);
-    }
+		@Override
+		public boolean isApplicable(@NotNull Project project)
+		{
+			return ModuleExtensionHelper.getInstance(project).hasModuleExtension(PyModuleExtension.class);
+		}
 
-    @Override
-    public String getName() {
-      return PyBundle.message("runcfg.nosetests.display_name");
-    }
-  }
+		@Override
+		public String getName()
+		{
+			return PyBundle.message("runcfg.pytest.display_name");
+		}
+	}
 
-  private static class PythonAtTestConfigurationFactory extends ConfigurationFactory {
-    protected PythonAtTestConfigurationFactory(ConfigurationType configurationType) {
-      super(configurationType);
-    }
+	private static class PythonNoseTestConfigurationFactory extends ConfigurationFactory
+	{
+		protected PythonNoseTestConfigurationFactory(ConfigurationType configurationType)
+		{
+			super(configurationType);
+		}
 
-    @Override
-    public RunConfiguration createTemplateConfiguration(Project project) {
-      return new PythonAtTestRunConfiguration(project, this);
-    }
+		@Override
+		public RunConfiguration createTemplateConfiguration(Project project)
+		{
+			return new PythonNoseTestRunConfiguration(project, this);
+		}
 
-    @Override
-    public String getName() {
-      return PyBundle.message("runcfg.attest.display_name");
-    }
-  }
+		@Override
+		public boolean isApplicable(@NotNull Project project)
+		{
+			return ModuleExtensionHelper.getInstance(project).hasModuleExtension(PyModuleExtension.class);
+		}
 
-  @Override
-  public String getDisplayName() {
-    return PyBundle.message("runcfg.test.display_name");
-  }
+		@Override
+		public String getName()
+		{
+			return PyBundle.message("runcfg.nosetests.display_name");
+		}
+	}
 
-  @Override
-  public String getConfigurationTypeDescription() {
-    return PyBundle.message("runcfg.test.description");
-  }
+	private static class PythonAtTestConfigurationFactory extends ConfigurationFactory
+	{
+		protected PythonAtTestConfigurationFactory(ConfigurationType configurationType)
+		{
+			super(configurationType);
+		}
 
-  @Override
-  public Icon getIcon() {
-    return PythonIcons.Python.PythonTests;
-  }
+		@Override
+		public RunConfiguration createTemplateConfiguration(Project project)
+		{
+			return new PythonAtTestRunConfiguration(project, this);
+		}
 
-  @NotNull
-  @Override
-  public String getId() {
-    return ID;
-  }
+		@Override
+		public boolean isApplicable(@NotNull Project project)
+		{
+			return ModuleExtensionHelper.getInstance(project).hasModuleExtension(PyModuleExtension.class);
+		}
 
-  @Override
-  public ConfigurationFactory[] getConfigurationFactories() {
-    return new ConfigurationFactory[] {PY_UNITTEST_FACTORY, PY_DOCTEST_FACTORY, PY_NOSETEST_FACTORY,
-        PY_PYTEST_FACTORY, PY_ATTEST_FACTORY};
-  }
+		@Override
+		public String getName()
+		{
+			return PyBundle.message("runcfg.attest.display_name");
+		}
+	}
+
+	@Override
+	public String getDisplayName()
+	{
+		return PyBundle.message("runcfg.test.display_name");
+	}
+
+	@Override
+	public String getConfigurationTypeDescription()
+	{
+		return PyBundle.message("runcfg.test.description");
+	}
+
+	@Override
+	public Icon getIcon()
+	{
+		return PythonIcons.Python.PythonTests;
+	}
+
+	@NotNull
+	@Override
+	public String getId()
+	{
+		return ID;
+	}
+
+	@Override
+	public ConfigurationFactory[] getConfigurationFactories()
+	{
+		return new ConfigurationFactory[]{
+				PY_UNITTEST_FACTORY,
+				PY_DOCTEST_FACTORY,
+				PY_NOSETEST_FACTORY,
+				PY_PYTEST_FACTORY,
+				PY_ATTEST_FACTORY
+		};
+	}
 }
