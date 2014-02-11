f90e446f05683d4d05ebecc27f8590025dd05679
VISTALL <vistall.valeriy@gmail.com>
2014-02-11 11:28:04 +0200

    impl pymoduleextension

--- jython/src/org/consulo/jython/module/extension/JythonModuleExtension.java
+++ jython/src/org/consulo/jython/module/extension/JythonModuleExtension.java
@@ -1,6 +1,7 @@
 package org.consulo.jython.module.extension;
 
 import org.consulo.module.extension.impl.ModuleExtensionWithSdkImpl;
+import org.consulo.python.module.extension.PyModuleExtension;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.projectRoots.SdkType;
@@ -10,7 +11,7 @@ import com.jetbrains.python.sdk.PythonSdkType;
  * @author VISTALL
  * @since 25.10.13.
  */
-public class JythonModuleExtension extends ModuleExtensionWithSdkImpl<JythonModuleExtension>
+public class JythonModuleExtension extends ModuleExtensionWithSdkImpl<JythonModuleExtension> implements PyModuleExtension<JythonModuleExtension>
 {
 	public JythonModuleExtension(@NotNull String id, @NotNull Module module)
 	{
