62016fd0ee4244fcb4a92386bb19ab389477f711
VISTALL <vistall.valeriy@gmail.com>
2014-06-03 09:47:29 +0300

    iron python plugin

---
+++ iron-python/src/org/mustbe/consulo/ironPython/module/extension/BaseIronPythonModuleExtension.java
@@ -0,0 +1,11 @@
+package org.mustbe.consulo.ironPython.module.extension;
+
+import org.consulo.python.module.extension.PyModuleExtension;
+
+/**
+ * @author VISTALL
+ * @since 03.06.14
+ */
+public interface BaseIronPythonModuleExtension<T extends BaseIronPythonModuleExtension<T>> extends PyModuleExtension<T>
+{
+}
