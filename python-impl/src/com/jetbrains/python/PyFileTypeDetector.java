c959ce6e0ed43e742eba3fa6c6e4486e62225066
VISTALL <vistall.valeriy@gmail.com>
2015-08-09 09:46:52 +0300

    compilation

--- python-impl/src/com/jetbrains/python/PyFileTypeDetector.java
+++ python-impl/src/com/jetbrains/python/PyFileTypeDetector.java
@@ -16,20 +16,28 @@
 
 package com.jetbrains.python;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.FileTypeRegistry;
 import com.intellij.openapi.util.io.ByteSequence;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * @author yole
  */
-public class PyFileTypeDetector implements FileTypeRegistry.FileTypeDetector {
-  @Override
-  public FileType detect(@NotNull VirtualFile file, @NotNull ByteSequence firstBytes, @Nullable CharSequence firstCharsIfText) {
-    return FileUtil.isHashBangLine(firstCharsIfText, "python") ? PythonFileType.INSTANCE : null;
-  }
+public class PyFileTypeDetector implements FileTypeRegistry.FileTypeDetector
+{
+	@Override
+	public FileType detect(@NotNull VirtualFile file, @NotNull ByteSequence firstBytes, @Nullable CharSequence firstCharsIfText)
+	{
+		return FileUtil.isHashBangLine(firstCharsIfText, "python") ? PythonFileType.INSTANCE : null;
+	}
+
+	@Override
+	public int getVersion()
+	{
+		return 1;
+	}
 }
