e06fe36206fbb7835dca2a61e64611d81db03b68
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-01-13 20:24:41 +0400

    remove impl usage

--- plugin/src/main/java/module-info.java
+++ plugin/src/main/java/module-info.java
@@ -6,7 +6,4 @@ module consulo.python {
   requires consulo.ide.api;
   requires consulo.python.language.api;
   requires consulo.python.impl;
-
-  // TODO remove in future
-  requires consulo.ide.impl;
 }
