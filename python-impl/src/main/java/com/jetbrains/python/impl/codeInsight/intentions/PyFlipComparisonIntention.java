108bd6a87ea7b2d1985d88f28273da1f0482eb31
UNV <nikolay@yurchenko.su>
2025-10-10 20:59:18 +0300

    Removing PyFlipComparisonIntention.getFamilyName. Refactoring.

--- python-impl/src/main/java/com/jetbrains/python/impl/codeInsight/intentions/PyFlipComparisonIntention.java
+++ python-impl/src/main/java/com/jetbrains/python/impl/codeInsight/intentions/PyFlipComparisonIntention.java
@@ -30,7 +30,6 @@ import consulo.project.Project;
 import consulo.python.impl.localize.PyLocalize;
 import jakarta.annotation.Nonnull;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -38,22 +37,15 @@ import java.util.Map;
  * @since 2010-03-26
  */
 public class PyFlipComparisonIntention extends BaseIntentionAction {
-    private static final Map<PyElementType, String> FLIPPED_OPERATORS = new HashMap<PyElementType, String>(7);
-
-    static {
-        FLIPPED_OPERATORS.put(PyTokenTypes.EQEQ, "==");
-        FLIPPED_OPERATORS.put(PyTokenTypes.NE, "!=");
-        FLIPPED_OPERATORS.put(PyTokenTypes.NE_OLD, "<>");
-        FLIPPED_OPERATORS.put(PyTokenTypes.GE, "<=");
-        FLIPPED_OPERATORS.put(PyTokenTypes.LE, ">=");
-        FLIPPED_OPERATORS.put(PyTokenTypes.GT, "<");
-        FLIPPED_OPERATORS.put(PyTokenTypes.LT, ">");
-    }
-
-    @Nonnull
-    public String getFamilyName() {
-        return PyLocalize.intnFlipComparison().get();
-    }
+    private static final Map<PyElementType, String> OUR_FLIPPED_OPERATORS = Map.of(
+        PyTokenTypes.EQEQ, "==",
+        PyTokenTypes.NE, "!=",
+        PyTokenTypes.NE_OLD, "<>",
+        PyTokenTypes.GE, "<=",
+        PyTokenTypes.LE, ">=",
+        PyTokenTypes.GT, "<",
+        PyTokenTypes.LT, ">"
+    );
 
     public boolean isAvailable(@Nonnull Project project, Editor editor, PsiFile file) {
         if (!(file instanceof PyFile)) {
@@ -64,9 +56,9 @@ public class PyFlipComparisonIntention extends BaseIntentionAction {
         PyBinaryExpression binaryExpression = PsiTreeUtil.getParentOfType(element, PyBinaryExpression.class, false);
         while (binaryExpression != null) {
             PyElementType operator = binaryExpression.getOperator();
-            if (FLIPPED_OPERATORS.containsKey(operator)) {
+            if (OUR_FLIPPED_OPERATORS.containsKey(operator)) {
                 String operatorText = binaryExpression.getPsiOperator().getText();
-                String flippedOperatorText = FLIPPED_OPERATORS.get(operator);
+                String flippedOperatorText = OUR_FLIPPED_OPERATORS.get(operator);
                 if (flippedOperatorText.equals(operatorText)) {
                     setText(PyLocalize.intnFlip$0(operatorText));
                 }
@@ -84,10 +76,10 @@ public class PyFlipComparisonIntention extends BaseIntentionAction {
         PsiElement element = file.findElementAt(editor.getCaretModel().getOffset());
         PyBinaryExpression binaryExpression = PsiTreeUtil.getParentOfType(element, PyBinaryExpression.class, false);
         while (binaryExpression != null) {
-            if (FLIPPED_OPERATORS.containsKey(binaryExpression.getOperator())) {
+            if (OUR_FLIPPED_OPERATORS.containsKey(binaryExpression.getOperator())) {
                 PyElementGenerator elementGenerator = PyElementGenerator.getInstance(project);
                 binaryExpression.replace(elementGenerator.createBinaryExpression(
-                    FLIPPED_OPERATORS.get(binaryExpression.getOperator()),
+                    OUR_FLIPPED_OPERATORS.get(binaryExpression.getOperator()),
                     binaryExpression.getRightExpression(),
                     binaryExpression.getLeftExpression()
                 ));
